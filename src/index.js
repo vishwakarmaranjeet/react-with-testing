b6c3ce3fa6923e47a00264fd5cb77dc275fd1b07
Ranjeet Vishwakarma <rvishwakarma0099@gmail.com>
2023-10-02 22:43:31 +0530

    used code removed

--- src/index.js
+++ src/index.js
@@ -11,15 +11,6 @@ Sentry.init({
   autoSessionTracking: true,
   integrations: [new Sentry.BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  tracesSampleRate: 1.0,
-
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
