2f71a6968fdaa1373e59b4ad35e9e7de34b52e9f
Ranjeet Vishwakarma <rvishwakarma0099@gmail.com>
2024-01-01 18:31:23 +0530

    test cases updated

--- src/__tests__/componets/DropDown.test.js
+++ src/__tests__/componets/DropDown.test.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import DropDown from "../../components/DropDown";
 
-test("Input component should render", ()=> {
+test("It should render correctlys", ()=> {
     const { getByText } = render(<DropDown />);
     const dropDownText = getByText(/Select an option/);
     expect(dropDownText).toBeInTheDocument();
 });
 
-test("Input component should render", ()=> {
+test("It render and submit button should be disabled", ()=> {
+    const { getByText } = render(<DropDown />);
+    const submitBtn = getByText(/submit/i);
+    expect(submitBtn).toBeInTheDocument();
+    expect(submitBtn).toBeDisabled();
+});
+
+test("It render input field has some value and submit button should not be disabled", ()=> {
     const { getByRole, getByText } = render(<DropDown />);
     const dropDownElement = getByRole("combobox");
     fireEvent.change(dropDownElement, { target: { value: "India" }});
--- src/__tests__/componets/Input.test.js
+++ src/__tests__/componets/Input.test.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import InputBox from "../../components/Input";
 
-test("Input component should render", ()=> {
+test("It should render", ()=> {
     const { getByPlaceholderText } = render(<InputBox/>);
     const inputElement = getByPlaceholderText("Enter your name");
     expect(inputElement).toBeInTheDocument();
 });
 
-test("Input component should render", ()=> {
+test("It render and click me button should be disabled", ()=> {
+    const { getByText } = render(<InputBox/>);
+    const btnElement = getByText(/click me/i);
+    expect(btnElement).toBeInTheDocument();
+    expect(btnElement).toBeDisabled();
+});
+
+test("It render and input has some value and click me button should not be disable", ()=> {
     const { getByPlaceholderText, getByText } = render(<InputBox/>);
     const inputElement = getByPlaceholderText("Enter your name");
     fireEvent.change(inputElement, { target: { value: "Test input" }});
-    expect(inputElement.value).toBe("Test input");
+    expect(inputElement).toHaveValue("Test input");
+
     const btnElement = getByText(/click me/i);
     expect(btnElement).toBeInTheDocument();
     expect(btnElement).not.toBeDisabled();
