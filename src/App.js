fbcba273a9d4b3733ca06c3d561d92d98eceec01
Ranjeet Vishwakarma <rvishwakarma0099@gmail.com>
2024-04-29 21:31:27 +0530

    API added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import React, { useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 import { Greet } from "./components/greet/greet";
 import InputBox from "./components/Input";
 import DropDown from "./components/DropDown";
 
 function App() {
+  const checkUserLogin = async () => {
+    try {
+      const response = await axios.post('https://vxpress.vijayant.in/integration/users/login.json', {
+          "user": {
+            "email": "ASHIRWAD_API",
+            "password": "123456",
+            // "consignment_type": "OUTOBOUND-TBB"
+          }
+      });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
   return (
     <>
-      <br />
+      {/* <br />
       <Greet name="World" /> <br />
       <InputBox /> <br /><br />
-      <DropDown />
+      <DropDown /> */}
       <div className="App">
+      <button onClick={checkUserLogin}>Call API</button>
       </div>
     </>
   );
